9ebc0a9aaf0355449a70ef43006e78ca68b25f5c
Diego Brito <dibbrito@gmail.com>
2024-01-29 23:38:13 -0300

    fix nome application

--- src/main/java/br/com/fiap/produto/ProdutoApplication.java
+++ src/main/java/br/com/fiap/produto/ProdutoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PedidoApplication {
+public class ProdutoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PedidoApplication.class, args);
+		SpringApplication.run(ProdutoApplication.class, args);
 	}
 
 }
